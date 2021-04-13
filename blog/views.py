0e7b8dd8db6b7600b272642b3de81cf95f5e29bb
dimaefimov <klimmerst@gmail.com>
2021-04-13 22:51:12 +0300

    Добавил get, post с APIView

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,27 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from blog.models import Note
+from blog.serializers import NoteSerializer
+
+
+class NoteView(APIView):
+
+    def get(self, request):
+        notes = Note.objects.all()
+        serialized_notes = NoteSerializer(notes, many=True)
+
+        return Response(serialized_notes.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serialized_notes = NoteSerializer(data=request.data)
+
+        if serialized_notes.is_valid():
+            serialized_notes.save(author=request.user)
+            return Response(serialized_notes.data, status=status.HTTP_201_CREATED)
+
+        return Response(serialized_notes.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
