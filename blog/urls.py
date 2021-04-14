27b1dec917e1ca4d95847b8d7d37db847629c58a
dimaefimov <klimmerst@gmail.com>
2021-04-15 01:38:47 +0300

    Добавил path для списка комментариев и одного комментария

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import AllNotesView, SingleNoteView
+from .views import AllNotesView, SingleNoteView, AllCommentsView, SingleCommentView
 
 app_name = 'blog'
 urlpatterns = [
     path('note/', AllNotesView.as_view(), name='notes'),
     path('note/<int:note_id>', SingleNoteView.as_view(), name='single_note'),
+    path('comment/', AllCommentsView.as_view(), name='notes'),
+    path('comment/<int:comment_id>', SingleCommentView.as_view(), name='single_note'),
 ]
