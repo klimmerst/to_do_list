db06867035515fa3195cb5e0d7b4695af04a1af2
dimaefimov <klimmerst@gmail.com>
2021-04-14 03:17:44 +0300

    SingleNoteView для запросов из note/note_id

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.exceptions import NotFound
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from blog.models import Note
-from blog.serializers import NoteSerializer
+from .models import Note
+from .serializers import NoteSerializer
 
 
-class NoteView(APIView):
+class AllNotesView(APIView):
+    permission_classes = (IsAuthenticated,)
+
 
     def get(self, request):
-        notes = Note.objects.all()
+        notes = Note.objects.filter(public=True)
         serialized_notes = NoteSerializer(notes, many=True)
 
         return Response(serialized_notes.data, status=status.HTTP_200_OK)
@@ -23,5 +27,35 @@ class NoteView(APIView):
             return Response(serialized_notes.data, status=status.HTTP_201_CREATED)
 
         return Response(serialized_notes.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SingleNoteView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, note_id):
+        note = Note.objects.filter(pk=note_id).first()
+        serialized_note = NoteSerializer(note)
+
+        return Response(serialized_note.data)
+
+    def patch(self, request, note_id):
+        note = Note.objects.filter(pk=note_id, author=request.user).first()
+
+        if not note:
+            raise NotFound(f'Статья {note_id} у пользователя {request.user.username} не найдена')
+
+        serialized_note = NoteSerializer(note, data=request.data, partial=True)
+
+        if serialized_note.is_valid():
+            serialized_note.save()
+            return Response(serialized_note.data, status=status.HTTP_200_OK)
+
+        return Response(serialized_note.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, note_id):
+        note = Note.objects.filter(pk=note_id, author=request.user).first()
+        note.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
