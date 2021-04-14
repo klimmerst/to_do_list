6bbf5009faabe24473ed3987b338856f6342e7b2
dimaefimov <klimmerst@gmail.com>
2021-04-15 01:30:38 +0300

    Сделал комментарии для админки

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.forms import models
 
-from blog.models import Note
+from blog.models import Note, Comment
 
 
 @admin.register(Note)
@@ -10,7 +10,19 @@ class NoteAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'text', 'date_add', 'importance', 'public', 'state', )
     fields = (('title', 'state', ), 'text', ('importance', 'public', ), )
     readonly_fields = ('date_add', )
-    list_filter = ('importance', 'state', 'public')
+    list_filter = ('importance', 'state', 'public', )
+
+    def save_model(self, request, obj, form, change):
+        if not hasattr(obj, "author") or not obj.author:
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'message', 'date_add', 'note', )
+    fields = ('message', 'note', )
+    readonly_fields = ('date_add', )
 
     def save_model(self, request, obj, form, change):
         if not hasattr(obj, "author") or not obj.author:
