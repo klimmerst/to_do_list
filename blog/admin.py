da07d50dd4fe522e1e452e7568a78583fdc1924b
dimaefimov <klimmerst@gmail.com>
2021-04-14 03:08:46 +0300

    Сгруппировал параметры

--- blog/admin.py
+++ blog/admin.py
@@ -6,15 +6,13 @@ from blog.models import Note
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'text', 'date_add', 'importance', 'privacy', 'state', )
 
-    fields = (('title', 'state', ), 'text', ('importance', 'privacy', ), )
-
+    list_display = ('id', 'title', 'text', 'date_add', 'importance', 'public', 'state', )
+    fields = (('title', 'state', ), 'text', ('importance', 'public', ), )
     readonly_fields = ('date_add', )
-
-    list_filter = ('importance', 'state', 'privacy')
+    list_filter = ('importance', 'state', 'public')
 
     def save_model(self, request, obj, form, change):
-        if not hasattr(obj, "author") or not obj.user:
+        if not hasattr(obj, "author") or not obj.author:
             obj.author = request.user
         super().save_model(request, obj, form, change)
